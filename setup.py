5ce57fe71b60f5f617b1135c43d0a637786184ad
Gytis <gytis@tripcreator.com>
2017-11-28 07:21:54 +0200

    let's bump this to 1.0.0

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
-setup(
-    name = "thumbor_google_cloud",
-    version = "0.0.1",
-    author = "Tripcreator Ehf.",
-    author_email = "None",
-    description = "Simple storage (non result) for Thumbor on Google Cloud",
-    license = "MIT",
-    keywords = "thumbor google cloud",
-    url = "https://github.com/xperious/thumbor-google-cloud",
-    packages=['thumbor_google_cloud'],
-    install_requires =  [
-      "thumbor>=6.3.0",
-      "google-cloud-storage>=1.6.0",
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "License :: OSI Approved :: MIT License"
-    ],
-)
+if __name__ == "__main__":
+    setup(
+        name = "thumbor_google_cloud",
+        version = "1.0.0",
+        author = "Tripcreator Ehf.",
+        author_email = "None",
+        description = "Simple storage (non result) for Thumbor on Google Cloud",
+        license = "MIT",
+        keywords = "thumbor google cloud",
+        url = "https://github.com/xperious/thumbor-google-cloud",
+        packages=['thumbor_google_cloud'],
+        install_requires =  [
+        "thumbor>=6.3.0",
+        "google-cloud-storage>=1.6.0",
+        ],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: MIT License"
+        ],
+    )
